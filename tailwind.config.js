ac68b7017cef6506a5c4ba11748615b17ff39009
abdullah-shah-5 <ashace61@gmail.com>
2022-01-13 10:50:49 +0500

    commit after installing tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./assets/*.liquid",
+  "./layout/*.liquid",
+  "./sections/*.liquid",
+  "./snippets/*.liquid",
+  "./templates/*.liquid",
+  "./templates/customers/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
